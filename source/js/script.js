ef4f33b4ce00faed307873ef3bf22f1910ada390
Tatyana <taniaks2003@mail.ru>
2018-10-28 01:56:41 +0500

    Завершена работа с модалкой

--- source/js/script.js
+++ source/js/script.js
@@ -1,28 +1,26 @@
-var link = document.querySelector(".product-week__order");
-var link2 = document.querySelector(".catalog__icon-cart");
+var modal = document.querySelectorAll(".js-modal-show");
 var popup = document.querySelector(".modal");
 var PopupClose = popup.querySelector(".modal__button");
 
-link.addEventListener("click", function (evt) {
-  evt.preventDefault();
-  popup.classList.add("modal-show");
-});
-
-link2.addEventListener("click", function (evt) {
-  evt.preventDefault();
-  popup.classList.add("modal-show");
-});
-
-PopupClose.addEventListener("click", function (evt) {
-  evt.preventDefault();
-  popup.classList.remove("modal-show");
-});
-
-window.addEventListener("keydown", function (evt) {
-  if (evt.keyCode === 27) {
-    evt.preventDefault();
-    if (popup.classList.contains("modal-show")) {
-      popup.classList.remove("modal-show");
-    }
+if (modal) {
+  for (var i = 0; i < modal.length; i++){
+    modal[i].addEventListener("click", function (evt) {
+      evt.preventDefault();
+      popup.classList.add("modal-show");
+    });
   }
-});
+
+  PopupClose.addEventListener("click", function (evt) {
+    evt.preventDefault();
+    popup.classList.remove("modal-show");
+  });
+
+  window.addEventListener("keydown", function (evt) {
+    if (evt.keyCode === 27) {
+      evt.preventDefault();
+      if (popup.classList.contains("modal-show")) {
+        popup.classList.remove("modal-show");
+      }
+    }
+  });
+}
